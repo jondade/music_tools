df7a402337bb6129e87eac993b15a2cc0c737aeb
Jonathan Dade <jonathandade@gmail.com>
2018-09-02 22:24:53 +0100

    Now with Rakefile for Travis

--- lib/music_tools.rb
+++ lib/music_tools.rb
@@ -5,7 +5,7 @@ class MusicTools
   # @param file [String] the (full path) and name of the file to organise
   # @return [boolean] if the file was successfully organised.
   # TODO add a feature to inspect changes that would be made.
-  def self.sort(directory, file)
+  def sort(directory, file)
     raise(ArgumentError, 'Missing arguments') if directory == '' || file == ''
     raise('Missing file') unless File.exist?(file)
     raise('No such directory') unless
